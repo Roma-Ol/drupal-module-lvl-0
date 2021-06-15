4915e1cba71b26d7c0ca3cc846e78c6c53ef669c
RomaRoma49 <v12nes11@gmail.com>
2021-06-15 17:56:09 +0300

    tsk-7 almost done

--- web/modules/custom/romaroma/src/Form/FormCats.php
+++ web/modules/custom/romaroma/src/Form/FormCats.php
@@ -7,6 +7,7 @@ use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Ajax\InsertCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Element\Form;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 // Класс отвечает за обработку данных
@@ -31,15 +32,29 @@ class FormCats extends FormBase {
       '#type' => 'textfield',
       '#title' => 'Your cat’s name:',
       '#description' => $this->t('max - 2 / min - 32'),
-//      '#required' => TRUE,
+      '#placeholder' => 'Name',
+      //      '#required' => TRUE,
       '#ajax' => [
         'callback' => '::validateNameAjax',
-//        'event' => 'change',
+        //        'event' => 'change',
         'progress' => [
           'type' => 'throbber',
           'message' => t('Verifying email..'),
         ],
       ]
+    ];
+
+    $form['email'] = [
+      '#title' => 'Your Email:',
+      '#type' => 'email',
+      '#description' => $this->t('allowed values: Aa-Zz / _ / -'),
+      '#required' => TRUE,
+      '#placeholder' => 'Email',
+      '#ajax' => [
+        'callback' => '::validateEmailAjax',
+        'event' => 'keyup',
+      ],
+      '#suffix' => '<div class="email-validation-message"></div>',
     ];
 
     $form['system_messages'] = [
@@ -55,14 +70,26 @@ class FormCats extends FormBase {
       '#ajax' => [
         'callback' => '::ajaxSubmitCallback',
         'event' => 'click'
-      ]
+      ],
     ];
     return $form;
   }
 
+  // EMAIL VALIDATION with dynamic text appearing
+  public function validateEmailAjax(array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    $value = $form_state->getValue('email');
+    if (filter_var($value, FILTER_VALIDATE_EMAIL) && !preg_match('/[#$%^&*()+=!\[\]\';,\/{}|":<>?~\\\\]/', $value)) {
+      $response->addCommand(new HtmlCommand('.email-validation-message', '<p class="email-input-message"></p>'));
+    } else {
+      $response->addCommand(new HtmlCommand('.email-validation-message', '<p class="email-input-message">sry, but we can`t find this email</p>'));
+    }
+    return $response;
+  }
+
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $value = $form_state->getValue('title');
-    if (!preg_match('/^[A-Za-z]*$/', $value) || strlen($value)<2 || strlen($value)>32) {
+    if (!preg_match('/^[A-Za-z]*$/', $value) || strlen($value)<2) {
       $form_state->setErrorByName ('title', t('The name %name is not valid.', array('%name' => $value)));
     }
   }
