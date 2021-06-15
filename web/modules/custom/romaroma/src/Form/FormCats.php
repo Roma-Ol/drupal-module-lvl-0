75bbcd0fbf88f0c90bf562b34fb56f402fb7d0c4
RomaRoma49 <v12nes11@gmail.com>
2021-06-15 12:45:01 +0300

    task 6 - done

--- web/modules/custom/romaroma/src/Form/FormCats.php
+++ web/modules/custom/romaroma/src/Form/FormCats.php
@@ -2,14 +2,25 @@
 
 namespace Drupal\romaroma\Form;
 
-use Drupal\Core\Form\FormBase;                         // Базовый класс Form API
-use Drupal\Core\Form\FormStateInterface;              // Класс отвечает за обработку данных
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\Core\Ajax\InsertCommand;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+// Класс отвечает за обработку данных
 
 /**
  * Наследуемся от базового класса Form API
  * @see \Drupal\Core\Form\FormBase
  */
 class FormCats extends FormBase {
+
+  // method that returns the form`s name
+  public function getFormId() {
+    return 'formCats';
+  }
 
   // method that creates a form
   public function buildForm(array $form, FormStateInterface $form_state) {
@@ -18,37 +29,64 @@ class FormCats extends FormBase {
 
     $form['title'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('What is the name of ur cat?'),
+      '#title' => 'Your cat’s name:',
       '#description' => $this->t('max - 2 / min - 32'),
-      '#required' => TRUE,
+//      '#required' => TRUE,
+      '#ajax' => [
+        'callback' => '::validateNameAjax',
+//        'event' => 'change',
+        'progress' => [
+          'type' => 'throbber',
+          'message' => t('Verifying email..'),
+        ],
+      ]
+    ];
+
+    $form['system_messages'] = [
+      '#markup' => '<div id="form-system-messages"></div>',
+      '#weight' => -100,
     ];
 
     // Add a submit button that handles the submission of the form.
-    $form['actions']['submit'] = [
+    $form['submit'] = [
       '#type' => 'submit',
+      '#name' => 'submit',
       '#value' => $this->t('Add cat'),
+      '#ajax' => [
+        'callback' => '::ajaxSubmitCallback',
+        'event' => 'click'
+      ]
     ];
-
     return $form;
   }
 
-  // method that returns the form`s name
-  public function getFormId() {
-    return 'romaroma';
-  }
-
-  // validation
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $value = $form_state->getValue('title');
-    if (preg_match('/^[0-9]*$/', $value) || strlen($value)<=2 || strlen($value)>=32) {
-      $form_state->setErrorByName ('title', t('%title `s name isn`t correct, sry.', array('%title' => $value)));
+    if (!preg_match('/^[A-Za-z]*$/', $value) || strlen($value)<2 || strlen($value)>32) {
+      $form_state->setErrorByName ('title', t('The name %name is not valid.', array('%name' => $value)));
     }
   }
 
-  // after submit action
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $title = $form_state->getValue('title');
-    \Drupal::messenger()->addMessage(t('Welcome to the club, %title', ['%title' => $title]));
+  public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
+    $ajax_response = new AjaxResponse();
+    $message = [
+      '#theme' => 'status_messages',
+      '#message_list' => $this->messenger()->all(),
+      '#status_headings' => [
+        'status' => t('Status message'),
+        'error' => t('Error message'),
+        'warning' => t('Warning message'),
+      ],
+    ];
+    $messages = \Drupal::service('renderer')->render($message);
+    $ajax_response->addCommand(new HtmlCommand('#form-system-messages', $messages));
+    return $ajax_response;
   }
 
+  /**
+   *  (@inheritdoc).
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    \Drupal::messenger()->addMessage($this->t('Form Submitted Successfully'), 'status', TRUE);
+  }
 }
