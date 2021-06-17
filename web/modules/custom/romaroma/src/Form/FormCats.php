378764410d499dcd84c9515e749b6fa93b3c3432
RomaRoma49 <v12nes11@gmail.com>
2021-06-17 11:57:01 +0300

    tsk 7 - adding email input

--- web/modules/custom/romaroma/src/Form/FormCats.php
+++ web/modules/custom/romaroma/src/Form/FormCats.php
@@ -9,21 +9,14 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element\Form;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use function Drupal\Tests\Core\Render\callback;
 
-// Класс отвечает за обработку данных
-
-/**
- * Наследуемся от базового класса Form API
- * @see \Drupal\Core\Form\FormBase
- */
 class FormCats extends FormBase {
 
-  // method that returns the form`s name
   public function getFormId() {
     return 'formCats';
   }
 
-  // method that creates a form
   public function buildForm(array $form, FormStateInterface $form_state) {
 
     $form['#markup'] = '<p class="heading-text">Hello! You can add here a photo of your cat.</p>';
@@ -31,16 +24,12 @@ class FormCats extends FormBase {
     $form['title'] = [
       '#type' => 'textfield',
       '#title' => 'Your cat’s name:',
-      '#description' => $this->t('max - 2 / min - 32'),
+      '#description' => $this->t('A-Z / min-2 / max-32'),
       '#placeholder' => 'Name',
-      //      '#required' => TRUE,
+      '#required' => FALSE,
       '#ajax' => [
         'callback' => '::validateNameAjax',
-        //        'event' => 'change',
-        'progress' => [
-          'type' => 'throbber',
-          'message' => t('Verifying email..'),
-        ],
+         'event' => 'change',
       ]
     ];
 
@@ -48,7 +37,6 @@ class FormCats extends FormBase {
       '#title' => 'Your Email:',
       '#type' => 'email',
       '#description' => $this->t('allowed values: Aa-Zz / _ / -'),
-      '#required' => TRUE,
       '#placeholder' => 'Email',
       '#ajax' => [
         'callback' => '::validateEmailAjax',
@@ -75,45 +63,59 @@ class FormCats extends FormBase {
     return $form;
   }
 
-  // EMAIL VALIDATION with dynamic text appearing
-  public function validateEmailAjax(array &$form, FormStateInterface $form_state) {
-    $response = new AjaxResponse();
-    $value = $form_state->getValue('email');
-    if (filter_var($value, FILTER_VALIDATE_EMAIL) && !preg_match('/[#$%^&*()+=!\[\]\';,\/{}|":<>?~\\\\]/', $value)) {
-      $response->addCommand(new HtmlCommand('.email-validation-message', '<p class="email-input-message"></p>'));
-    } else {
-      $response->addCommand(new HtmlCommand('.email-validation-message', '<p class="email-input-message">sry, but we can`t find this email</p>'));
+    //WORKING VARIANT general form validation
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+      $nameValue = $form_state->getValue('title');
+      $emailValue = $form_state->getValue('email');
+      if (!preg_match('/^[A-Za-z]*$/', $nameValue) || strlen($nameValue)<=2 || strlen($nameValue)>32 || $nameValue = "") {
+        $form_state->setErrorByName ('title', t('The name %name is not valid.', array('%name' => $nameValue)));
+      } else {
+        // CLEARS THE MESSAGES BEFORE SUBMITTING
+        $this->messenger()->deleteAll();
+      }
+      if (!filter_var($emailValue, FILTER_VALIDATE_EMAIL) || !preg_match('/^[A-Za-z1-9-_]+[@]+[a-z]+[.]+[a-z]+$/', $emailValue)) {
+        $form_state->setErrorByName('email', t('the email %email is not valid.', array('%email' => $emailValue)));
+      } else {
+        // CLEARS THE MESSAGES BEFORE SUBMITTING
+        $this->messenger()->deleteAll();
+      }
     }
-    return $response;
-  }
 
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    $value = $form_state->getValue('title');
-    if (!preg_match('/^[A-Za-z]*$/', $value) || strlen($value)<2) {
-      $form_state->setErrorByName ('title', t('The name %name is not valid.', array('%name' => $value)));
+    // EMAIL VALIDATION - AJAX dynamic alerts
+    public function validateEmailAjax(array &$form, FormStateInterface $form_state) {
+      $response = new AjaxResponse();
+      $emailValue = $form_state->getValue('email');
+      if (!filter_var($emailValue, FILTER_VALIDATE_EMAIL) || !preg_match('/^[A-Za-z1-9-_]+[@]+[a-z]+[.]+[a-z]+$/', $emailValue)) {
+        $response->addCommand(new HtmlCommand('#form-system-messages', '
+            <div class="email-ajax-validation-alert">
+                <p class="email-ajax-validation-alert-text">
+                    Budy, ur Email isn`t ok
+                </p>
+            </div>'));
+      } else {
+        $response->addCommand(new HtmlCommand('#form-system-messages', ''));
+      }
+      return $response;
     }
-  }
 
-  public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
-    $ajax_response = new AjaxResponse();
-    $message = [
-      '#theme' => 'status_messages',
-      '#message_list' => $this->messenger()->all(),
-      '#status_headings' => [
-        'status' => t('Status message'),
-        'error' => t('Error message'),
-        'warning' => t('Warning message'),
-      ],
-    ];
-    $messages = \Drupal::service('renderer')->render($message);
-    $ajax_response->addCommand(new HtmlCommand('#form-system-messages', $messages));
-    return $ajax_response;
-  }
-
-  /**
-   *  (@inheritdoc).
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    \Drupal::messenger()->addMessage($this->t('Form Submitted Successfully'), 'status', TRUE);
-  }
+    // SUBMIT BUTTON CALLBACK - AJAX
+    public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
+      $response = new AjaxResponse();
+      $message = [
+        '#theme' => 'status_messages',
+        '#message_list' => $this->messenger()->all(),
+        '#status_headings' => [
+          'status' => t('Status message'),
+          'error' => t('Error message'),
+          'warning' => t('Warning message'),
+        ],
+      ];
+      $messages = \Drupal::service('renderer')->render($message);
+      $response->addCommand(new HtmlCommand('#form-system-messages', $messages));
+      return $response;
+    }
+    // SUBMIT BUTTON
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+      \Drupal::messenger()->addMessage($this->t('Form Submitted Successfully'), 'status', TRUE);
+    }
 }
